ebfa6eb7ccb96306163249df75b2bea7a7c9fd74
annnnangan <anna.ymngan@gmail.com>
2024-08-14 23:55:56 +0800

    Complete Home - Banner, Theme & Blog

--- main.js
+++ main.js
@@ -1,4 +1,24 @@
-import './assets/scss/all.scss';
-import 'bootstrap/dist/js/bootstrap.min.js';
+import "./assets/scss/all.scss";
+import "bootstrap/dist/js/bootstrap.min.js";
 
-console.log("Hello world!");
+var swiper = new Swiper(".blogSwiper", {
+  loop: true,
+  slidesPerView: 1.1,
+  grabCursor: true,
+  pagination: {
+    el: ".swiper-blog-pagination",
+    type: "fraction",
+  },
+  navigation: {
+    nextEl: ".swiper-button-blog-next",
+    prevEl: ".swiper-button-blog-prev",
+  },
+  spaceBetween: 24,
+  breakpoints: {
+    // when window width is >= 768px
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 48,
+    },
+  },
+});
