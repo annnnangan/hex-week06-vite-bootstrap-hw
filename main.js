8b917a20bdfde2739df06372a6a61bfe830fd8e6
annnnangan <anna.ymngan@gmail.com>
2024-08-17 22:04:03 +0800

    Complete homepage life

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 import "./assets/scss/all.scss";
 import "bootstrap/dist/js/bootstrap.min.js";
 
-var swiper = new Swiper(".blogSwiper", {
+var blogSwiper = new Swiper(".blogSwiper", {
   loop: true,
-  slidesPerView: 1.1,
+  autoplay: {
+    delay: 5000,
+  },
+  slidesPerView: 1,
   grabCursor: true,
   pagination: {
     el: ".swiper-blog-pagination",
@@ -22,3 +25,136 @@ var swiper = new Swiper(".blogSwiper", {
     },
   },
 });
+
+var hotTopicSwiper = new Swiper(".hotTopicSwiper", {
+  loop: true,
+  autoplay: {
+    delay: 5000,
+  },
+  slidesPerView: 1,
+  grabCursor: true,
+  spaceBetween: 24,
+});
+
+//Life Swiper Configuration
+
+var lifeSwiper;
+
+function createLifeSwiper() {
+  lifeSwiper = new Swiper(".lifeSwiper", {
+    breakpoints: {
+      // when window width is >= 768px
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 30,
+        loop: true,
+        autoplay: {
+          delay: 5000,
+        },
+
+        grabCursor: true,
+        navigation: {
+          nextEl: ".swiper-button-life-next",
+          prevEl: ".swiper-button-life-prev",
+        },
+      },
+      992: {
+        slidesPerView: 3,
+        spaceBetween: 48,
+        loop: true,
+        autoplay: {
+          delay: 5000,
+        },
+
+        grabCursor: true,
+        navigation: {
+          nextEl: ".swiper-button-life-next",
+          prevEl: ".swiper-button-life-prev",
+        },
+      },
+    },
+  });
+}
+
+const lifeCardContainer = document.querySelector(".lifeSwiper .swiper-wrapper");
+
+//Remove Swiper on Mobile
+function disableLifeSwiper() {
+  if (window.innerWidth >= 768) {
+    lifeCardContainer.classList.remove("d-flex");
+    lifeCardContainer.classList.remove("flex-column");
+    createLifeSwiper();
+  } else {
+    if (lifeSwiper) {
+      lifeSwiper.destroy();
+    }
+    lifeCardContainer.classList.add("d-flex");
+    lifeCardContainer.classList.add("flex-column");
+  }
+}
+
+disableLifeSwiper();
+window.addEventListener("resize", () => {
+  disableLifeSwiper();
+  btnSize();
+  disableProjectSwiper();
+});
+
+const button = document.querySelectorAll("button");
+function btnSize() {
+  if (window.innerWidth >= 768) {
+    button.forEach((btn) => {
+      btn.classList.add("btn-lg");
+    });
+  } else {
+    button.forEach((btn) => {
+      btn.classList.remove("btn-lg");
+    });
+  }
+}
+
+btnSize();
+
+// Project Swiper
+let projectSwiper;
+function createProjectSwiper() {
+  projectSwiper = new Swiper(".projectSwiper", {
+    slidesPerView: 1,
+    grabCursor: true,
+    spaceBetween: 24,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+    },
+  });
+}
+
+//Remove Project Swiper on Desktop
+const projectSwiperWrapper = document.querySelector(".project .swiper-wrapper");
+const projectSwiperContainer = document.querySelector(".projectSwiper");
+const projectSwiperItem = document.querySelectorAll(
+  ".project .swiper-wrapper .swiper-item"
+);
+function disableProjectSwiper() {
+  if (window.innerWidth >= 768) {
+    projectSwiperContainer.classList.remove("swiper");
+    createProjectSwiper();
+    projectSwiperWrapper.classList.add("row");
+    projectSwiperItem.forEach((item) => {
+      item.classList.add("card-item");
+      item.classList.remove("swiper-slide");
+    });
+  } else {
+    if (projectSwiper) {
+      projectSwiper.destroy();
+    }
+    projectSwiperContainer.classList.add("swiper");
+    projectSwiperWrapper.classList.remove("row");
+    projectSwiperItem.forEach((item) => {
+      item.classList.remove("card-item");
+      item.classList.add("swiper-slide");
+    });
+  }
+}
+
+disableProjectSwiper();
